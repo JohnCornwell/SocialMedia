fb08c3d72b04ab44c45ed0d31b811ee620cbb094
John Cornwell <cornwell7432@gmail.com>
2025-10-29 16:16:48 -0500

    added header and light/dark mode

---
+++ SocialMedia/src/app/header/header.component.ts
@@ -0,0 +1,39 @@
+import { Component, Renderer2, RendererFactory2 } from '@angular/core';
+
+@Component({
+  selector: 'header-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  private renderer: Renderer2;
+  private currentTheme: string = 'light';
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.setTheme(savedTheme);
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.setTheme('dark');
+    }
+  }
+
+  setTheme(theme: string) {
+    this.renderer.setAttribute(document.documentElement, 'data-bs-theme', theme);
+    this.currentTheme = theme;
+    localStorage.setItem('theme', theme);
+  }
+
+  toggleTheme() {
+    const newTheme = this.currentTheme === 'light' ? 'dark' : 'light';
+    this.setTheme(newTheme);
+  }
+
+  isDarkMode() {
+    return this.currentTheme != 'light';
+  }
+}
